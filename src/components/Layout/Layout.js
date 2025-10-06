9f0ffb9131eb50f2e8d92d510460585324c4e5e4
PetriD <dimtri.williams.6448@gmail.com>
2025-10-06 16:15:51 -0500

    Initial app setup.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Container } from "reactstrap";
 
 const Layout = ({ children }) => {
-  console.log(children);
+  // console.log(children);
   return <Container>{children}</Container>;
 };
 
--- src/pages/SpendingSheetList/SpendingSheetList.js
+++ src/pages/SpendingSheetList/SpendingSheetList.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import { Popover, PopoverBody, PopoverHeader, Button } from "reactstrap";
+
 const SpendingSheetList = () => {
-  return <div>SpendingSheetList</div>;
+  const root = document.querySelector(":root");
+
+  console.log(root);
+
+  return (
+    <>
+      <Button id="Popover1" type="button">
+        Launch Popover
+      </Button>
+      <Popover flip target="Popover1" toggle={function noRefCheck() {}}>
+        <PopoverHeader>Popover Title</PopoverHeader>
+        <PopoverBody>
+          Sed posuere consectetur est at lobortis. Aenean eu leo quam.
+          Pellentesque ornare sem lacinia quam venenatis vestibulum.
+        </PopoverBody>
+      </Popover>
+      <div>SpendingSheetList</div>
+    </>
+  );
 };
 
 export default SpendingSheetList;
